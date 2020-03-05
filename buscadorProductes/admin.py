36ac2549677a52b100b63fc91914d2a12afc42f3
robert <robertmunne15@gmail.com>
2020-03-05 09:22:07 +0100

    models

---
+++ buscadorProductes/admin.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.contrib import admin
+#admin.site.register(models.Restaurant)admin.site.register(models.Dish)
+
+# Register your models here.
+from buscadorProductes import models
+admin.site.register(models.Producto)
+admin.site.register(models.Pasillos)
+admin.site.register(models.Estanterias)
+admin.site.register(models.localilizacionActualProd)
---
+++ buscadorProductes/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+
+class Producto(models.Model):
+    EAN = models.CharField(max_length=13)
+    cantidad = models.IntegerField()
+    nombre = models.CharField(max_length=140)
+    descipcion = models.CharField(max_length=140)
+
+    def __str__(self):
+        return "EAN:{0}, Producto:{1}, Cantidad:{2}, Descripcion:{3}".format(self.EAN, self.nombre, self.cantidad,
+                                                                             self.descipcion)
+
+
+class Pasillos(models.Model):
+    n_pasillo = models.IntegerField()
+
+    def __str__(self):
+        return "Pasillo:{0}".format(self.n_pasillo)
+
+
+class Estanterias(models.Model):
+    pasillo = models.ForeignKey(Pasillos, on_delete=models.CASCADE, default=None)
+    n_estanteria = models.IntegerField()
+
+    def __str__(self):
+        return "Pasillo:{0}, Estanteria:{1}".format(self.pasillo.n_pasillo, self.n_estanteria)
+
+
+class localilizacionActualProd(models.Model):
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE, default=None)
+    estanteria = models.ForeignKey(Estanterias, on_delete=models.CASCADE, default=None)
+
+    def __str__(self):
+        return "EAN:{0}, Producto:{1}, Pasillo:{2}, Estanteria:{3}".format(self.producto.EAN, self.producto.nombre,
+                                                                           self.estanteria.pasillo,
+                                                                           self.estanteria.n_estanteria)
+
+
+#class Almacen(models.Model):
+#    ubicacion = models.ForeignKey()
