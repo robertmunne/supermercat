36b114816d3860c75b815f1acbc9e712df1f821f
jorgics <jna1@alumnes.udl.cat>
2020-03-29 20:36:32 +0200

    errores

--- buscadorProductes/urls.py
+++ buscadorProductes/urls.py
@@ -4,11 +4,9 @@ from django.contrib import admin
 from .views import search
 from .views import login_view
 from .views import home
-<<<<<<< HEAD
+
 from .views import register
-=======
 from .views import productes
->>>>>>> jorge
 
 urlpatterns = [
     url(r'search$', search),
